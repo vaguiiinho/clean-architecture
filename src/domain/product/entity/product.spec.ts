320499163b63e5621aeb0ac75d899e935ca36901
vaguiiinho <noc2tubaron@gmail.com>
2022-08-23 20:56:15 -0300

    customer and product yup validation

--- src/domain/product/entity/product.spec.ts
+++ src/domain/product/entity/product.spec.ts
@@ -13,13 +13,13 @@ describe("Product unit tests", () => {
     }).toThrowError("product: Name is required");
   });
 
-  
+
   it("should throw error when price is less than zero", () => {
     expect(() => {
       const product = new Product("123", "Name", -1);
     }).toThrowError("product: Price must be greater than zero");
   });
-  
+
   it("should throw error when name is and id are empty and when price is less than zero", () => {
     expect(() => {
       let product = new Product("", "", -1);
